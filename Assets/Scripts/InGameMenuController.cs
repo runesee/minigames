c9dd744c5565063249c5cce7ec1b4e83589bdd9f
runesee <runesee@stud.ntnu.no>
2026-01-27 16:33:39 +0100

    Merge branch '67-refactor-tag' of https://github.com/runesee/minigames into 67-refactor-tag

--- Assets/Scripts/InGameMenuController.cs
+++ Assets/Scripts/InGameMenuController.cs
@@ -159,7 +159,7 @@ public class InGameMenuController : MonoBehaviour
             EventSystem.current.SetSelectedGameObject(null);
         }
     }
-
+   
     public void ContinueGame()
     {
         ToggleMenu();
