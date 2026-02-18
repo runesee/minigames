40ccfad333c516df082c223801b5549995319d39
runesee <runesee@stud.ntnu.no>
2026-02-18 16:12:57 +0100

    feat: Make walls kinematic and update scene
    Add more walls, set longer toggle time, and update spawns accordingly.
    
    Relates: #116

---
+++ Assets/Scripts/TailTag/WallController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class WallController : NetworkBehaviour
+{
+    public List<GameObject> orangeWalls;
+    public List<GameObject> redWalls;
+    private bool wall_toggle = false;
+
+    public override void OnNetworkSpawn()
+    {
+        if (IsServer) StartCoroutine(WallRoutine());
+    }
+
+    IEnumerator WallRoutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(7f);
+            wall_toggle = !wall_toggle;
+            UpdateWallsClientRpc(wall_toggle);
+        }
+    }
+
+    private void UpdateWalls(bool wall_toggle)
+    {
+        foreach (var wall in orangeWalls)
+        {
+            wall.gameObject.SetActive(wall_toggle);
+        }
+        foreach (var wall in redWalls)
+        {
+            wall.gameObject.SetActive(!wall_toggle);
+        }
+    }
+
+    [ClientRpc]
+    void UpdateWallsClientRpc(bool wall_toggle)
+    {
+        UpdateWalls(wall_toggle);
+    }
+}
+
