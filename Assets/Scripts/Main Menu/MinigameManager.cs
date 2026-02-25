1626b525f5583b69fd47fb641c119ceb52162dfb
Magnus Brekke Nilsen <magnusbrekkenilsen@gmail.com>
2026-02-25 11:23:34 +0100

    Link Red Light and tutorial

--- Assets/Scripts/Main Menu/MinigameManager.cs
+++ Assets/Scripts/Main Menu/MinigameManager.cs
@@ -16,11 +16,12 @@ public class MinigameManager : NetworkBehaviour
         MainMenu,
         Lobby,
         Scoreboard,
-        RedLight,
         TagTutorial,
         Tag,
-        FocusFlow,
         FocusFlowTutorial,
+        FocusFlow,
+        RedLightTutorial,
+        RedLight
     }
 
     public MinigameScene currentGameState = MinigameScene.MainMenu;
@@ -37,8 +38,8 @@ public class MinigameManager : NetworkBehaviour
     public void StartGameSession()
     {
         if (!IsHost) return;
-        NetworkManager.Singleton.SceneManager.LoadScene("RedLight", LoadSceneMode.Single);
-        currentGameState = MinigameScene.RedLight;
+        NetworkManager.Singleton.SceneManager.LoadScene("TagTutorial", LoadSceneMode.Single);
+        currentGameState = MinigameScene.TagTutorial;
     }
 
     // General method called by each scene's GameState class once finished.
@@ -49,10 +50,6 @@ public class MinigameManager : NetworkBehaviour
         if (!(currentGameState == MinigameScene.Scoreboard)) previousGameState = currentGameState;
         switch (currentGameState)
         {
-            case MinigameScene.RedLight:
-                NetworkManager.Singleton.SceneManager.LoadScene("Scoreboard", LoadSceneMode.Single);
-                currentGameState = MinigameScene.Scoreboard;
-                break;
             case MinigameScene.TagTutorial:
                 NetworkManager.Singleton.SceneManager.LoadScene("TagScene", LoadSceneMode.Single);
                 currentGameState = MinigameScene.Tag;
@@ -61,26 +58,34 @@ public class MinigameManager : NetworkBehaviour
                 NetworkManager.Singleton.SceneManager.LoadScene("Scoreboard", LoadSceneMode.Single);
                 currentGameState = MinigameScene.Scoreboard;
                 break;
-            case MinigameScene.FocusFlow:
-                NetworkManager.Singleton.SceneManager.LoadScene("Scoreboard", LoadSceneMode.Single);
-                currentGameState = MinigameScene.Scoreboard;
-                break;
             case MinigameScene.FocusFlowTutorial:
                 NetworkManager.Singleton.SceneManager.LoadScene("FocusFlow", LoadSceneMode.Single);
                 currentGameState = MinigameScene.FocusFlow;
                 break;
+            case MinigameScene.FocusFlow:
+                NetworkManager.Singleton.SceneManager.LoadScene("Scoreboard", LoadSceneMode.Single);
+                currentGameState = MinigameScene.Scoreboard;
+                break;
+            case MinigameScene.RedLightTutorial:
+                NetworkManager.Singleton.SceneManager.LoadScene("RedLight", LoadSceneMode.Single);
+                currentGameState = MinigameScene.RedLight;
+                break;
+            case MinigameScene.RedLight:
+                NetworkManager.Singleton.SceneManager.LoadScene("Scoreboard", LoadSceneMode.Single);
+                currentGameState = MinigameScene.Scoreboard;
+                break;
             case MinigameScene.Scoreboard:
-                if (previousGameState == MinigameScene.RedLight)
-                {
-                    NetworkManager.Singleton.SceneManager.LoadScene("TagTutorial", LoadSceneMode.Single);
-                    currentGameState = MinigameScene.TagTutorial;
-                }
-                else if (previousGameState == MinigameScene.Tag)
+                if (previousGameState == MinigameScene.Tag)
                 {
                     NetworkManager.Singleton.SceneManager.LoadScene("FocusFlowTutorial", LoadSceneMode.Single);
                     currentGameState = MinigameScene.FocusFlowTutorial;
                 }
                 else if (previousGameState == MinigameScene.FocusFlow)
+                {
+                    NetworkManager.Singleton.SceneManager.LoadScene("RedLightTutorial", LoadSceneMode.Single);
+                    currentGameState = MinigameScene.RedLightTutorial;
+                }
+                else if (previousGameState == MinigameScene.RedLight)
                 {
                     NetworkManager.Singleton.SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
                     currentGameState = MinigameScene.MainMenu;
